05778b05eb7d39c016423138d3177482f788ed85
andrei_turobov <forgunn@yandex.ru>
2024-06-11 16:23:13 +0500

    added config files

---
+++ src/core/configs/__init__.py
@@ -0,0 +1,10 @@
+from core.configs.telegram import TelegramSettings
+
+
+class Settings(TelegramSettings):
+    class Config:
+        case_sensitive = True
+        env_file = '.env'
+
+
+settings = Settings()
---
+++ src/core/configs/telegram.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings
+
+
+class TelegramSettings(BaseSettings):
+   TELEGRAM_API_KEY: str
+   TELEGRAM_WEBHOOK_HOST: str
+   TELEGRAM_WEBHOOK_PATH: str
+
+   @property
+   def TELEGRAM_WEBHOOK_URL(self) -> str:
+       return f'{self.TELEGRAM_WEBHOOK_HOST}{self.TELEGRAM_WEBHOOK_PATH}'
